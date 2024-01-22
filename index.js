1a6f52dfc5b76a5acff47797162800d27e061dd7
Oluwafemi <oluwafemiomolounnu@gmail.com>
2024-01-22 13:50:20 +0100

    project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+let sendBtn = document.getElementById('sendBtn');
+
+sendBtn.addEventListener('click', function (e) {
+        alert('Message sent successfully!')
+
+});
